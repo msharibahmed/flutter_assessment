a801b0ee624ffa9301600a6aaada21af7ea58124
Sharib Ahmed <sharib@webmesecure.com>
2021-03-05 20:32:10 +0530

    added constant variables/values in separate folder

---
+++ lib/constants/constants.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+const profile_img =
+    "https://wgntv.com/wp-content/uploads/sites/5/2014/06/wgn-leshockmarcus-2250x2850.jpg";
+
+// ignore: non_constant_identifier_names
+const List<Color> grid_view_light_color =const [
+  Color(0xffffebee),
+  Color(0xffede7f6),
+  Color(0xffc8e6c9),
+  Color(0xffe1f5fe),
+];
+// ignore: non_constant_identifier_names
+ List<Color> grid_view_dark_color = [
+  Colors.red[300],
+  Colors.indigo[300],
+  Colors.green[300],
+  Colors.blue[300],
+];
+// ignore: non_constant_identifier_names
+const List<Color> grid_view_icon_color = const[
+  Colors.red,
+  Colors.indigo,
+  Colors.green,
+  Colors.blue,
+];
+// ignore: non_constant_identifier_names
+const List<String> grid_view_name =const ['Bill Pay', 'Rewards', 'Statement', 'Transfer'];
+// ignore: non_constant_identifier_names
+const List<String> grid_view_subname = const [
+  'Due on May 14th',
+  '12,324 Points',
+  'June 2020\nAvailable',
+  'Send & Request\nFunds'
+];
+// ignore: non_constant_identifier_names
+const List<IconData> grid_view_icon =const [
+  Icons.history_edu,
+  Icons.loyalty,
+  Icons.account_balance,
+  Icons.compare_arrows
+];
+
+// ignore: non_constant_identifier_names
+Map<IconData, String> bottom_navbar_name = {
+  Icons.dashboard: 'Dashboard',
+  Icons.qr_code_scanner: 'Scan Reciept',
+  Icons.payments: 'Send & Request',
+  Icons.account_circle_sharp: 'Settings'
+};
+
+
