19edc6dc1b2aebafa3e62480174316ec0d41e311
Sharib Ahmed <sharib@webmesecure.com>
2021-03-05 21:21:59 +0530

    added gridview item model

---
+++ lib/model/grid_view_model.dart
@@ -0,0 +1,13 @@
+//packages
+import 'package:flutter/material.dart';
+
+class GridViewModel {
+  final Color darkColor;
+  final Color lightColor;
+  final Color iconColor;
+  final String title;
+  final String subTitle;
+  final IconData icon;
+
+GridViewModel({@required this.darkColor,@required this.lightColor,@required this.iconColor,@required this.icon,@required this.title,@required this.subTitle})
+;}
