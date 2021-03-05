fdadf88d3d0e346032422c6fcb8a859c1a53856c
Sharib Ahmed <sharib@webmesecure.com>
2021-03-05 21:22:52 +0530

    changed gridview constants to gridview model

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,47 +1,45 @@
+//packages
 import 'package:flutter/material.dart';
 
-// ignore: non_constant_identifier_names
+//models
+import '../model/grid_view_model.dart';
+
+// top profile image
 const profile_img =
     "https://wgntv.com/wp-content/uploads/sites/5/2014/06/wgn-leshockmarcus-2250x2850.jpg";
 
-// ignore: non_constant_identifier_names
-const List<Color> grid_view_light_color =const [
-  Color(0xffffebee),
-  Color(0xffede7f6),
-  Color(0xffc8e6c9),
-  Color(0xffe1f5fe),
-];
-// ignore: non_constant_identifier_names
- List<Color> grid_view_dark_color = [
-  Colors.red[300],
-  Colors.indigo[300],
-  Colors.green[300],
-  Colors.blue[300],
-];
-// ignore: non_constant_identifier_names
-const List<Color> grid_view_icon_color = const[
-  Colors.red,
-  Colors.indigo,
-  Colors.green,
-  Colors.blue,
-];
-// ignore: non_constant_identifier_names
-const List<String> grid_view_name =const ['Bill Pay', 'Rewards', 'Statement', 'Transfer'];
-// ignore: non_constant_identifier_names
-const List<String> grid_view_subname = const [
-  'Due on May 14th',
-  '12,324 Points',
-  'June 2020\nAvailable',
-  'Send & Request\nFunds'
-];
-// ignore: non_constant_identifier_names
-const List<IconData> grid_view_icon =const [
-  Icons.history_edu,
-  Icons.loyalty,
-  Icons.account_balance,
-  Icons.compare_arrows
+List<GridViewModel> gridViewItem = [
+  GridViewModel(
+      lightColor: Color(0xffffebee),
+      darkColor: Colors.red[300],
+      iconColor: Colors.red,
+      title: 'Bill Pay',
+      subTitle: 'Due on May 14th',
+      icon: Icons.history_edu),
+  GridViewModel(
+      lightColor: Color(0xffede7f6),
+      darkColor: Colors.indigo[300],
+      iconColor: Colors.indigo,
+      title: 'Rewards',
+      subTitle: '12,324 Points',
+      icon: Icons.loyalty),
+  GridViewModel(
+      lightColor: Color(0xffc8e6c9),
+      darkColor: Colors.green[300],
+      iconColor: Colors.green,
+      title: 'Statement',
+      subTitle: 'June 2020\nAvailable',
+      icon: Icons.account_balance),
+  GridViewModel(
+      lightColor: Color(0xffe1f5fe),
+      darkColor: Colors.blue[300],
+      iconColor: Colors.blue,
+      title: 'Transfer',
+      subTitle: 'Send & Request\nFunds',
+      icon: Icons.compare_arrows),
 ];
 
+// bottom_navbar name & icon
 // ignore: non_constant_identifier_names
 Map<IconData, String> bottom_navbar_name = {
   Icons.dashboard: 'Dashboard',
@@ -49,5 +47,3 @@ Map<IconData, String> bottom_navbar_name = {
   Icons.payments: 'Send & Request',
   Icons.account_circle_sharp: 'Settings'
 };
-
-
